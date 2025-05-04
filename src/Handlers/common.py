c51cf5c91bf2d018ee5ca2440c34f22ce02c3894
Hecker <denis.ureev77@gmail.com>
2025-05-05 01:25:36 +0300

    тригонометрия

--- src/Handlers/common.py
+++ src/Handlers/common.py
@@ -1,25 +1,51 @@
 from aiogram.fsm.context import FSMContext
 
 from aiogram import Bot, Router, html, F
-from aiogram.filters import CommandStart, CommandObject, Command
+from aiogram.filters import CommandStart, CommandObject, Command, StateFilter
 from aiogram.types import Message, LabeledPrice, PreCheckoutQuery
+from aiogram.utils.formatting import as_list, Bold, BlockQuote, Text
 
 from src.classes import keyboards
+from src.classes import states
 
 router = Router(name="commnon")
 
 
 
+@router.message(Command("start"))
 @router.message(CommandStart(deep_link=True))
 async def command_start_handler(message: Message, command: CommandObject, state: FSMContext) -> None:
     await state.clear()
 
     await message.reply("Привет!")
+
     await message.answer("Вот меню:", reply_markup=keyboards.main_menu())
+    await state.set_state(states.CommonStates.main_menu)
+
+@router.message(Command("menu"))
+@router.message(F.text.lower() == "меню")
+async def menu_command_handler(message: Message, state: FSMContext) -> None:
+    await state.clear()
+
+    await message.answer("Вот меню:", reply_markup=keyboards.main_menu())
+    await state.set_state(states.CommonStates.main_menu)
+
+
+@router.message(states.CommonStates.main_menu, F.text.lower() == "о нас")
+async def about_command_handler(message: Message, state: FSMContext) -> None:
+
+    await message.reply(**as_list(
+        BlockQuote(Bold("PLACEHOLDER")),
+        Text("Lorem ipsum dolor sit amed.")
+    ).as_kwargs(), reply_markup=keyboards.main_menu())
+
+    await state.set_state(states.CommonStates.main_menu)
+
 
 @router.message(Command("cancel"))
-@router.message(F.text.lower == "отмена")
-async def cancel_handler(message: Message, command: CommandObject, state: FSMContext) -> None:
+@router.message(F.text.lower() == "отмена")
+@router.message(F.text.lower() == "назад")
+async def cancel_handler(message: Message, state: FSMContext) -> None:
     await state.clear()
 
 
@@ -34,3 +60,9 @@ async def cancel_handler(message: Message, command: CommandObject, state: FSMCon
     #                              ],
     #                              provider_token="1744374395:TEST:2c5a6f30c2763af47ad6")
 
+# Если нет состояния
+@router.message(StateFilter(None))
+async def base_handler(message: Message, state: FSMContext) -> None:
+    await message.reply("Упс! Прости, мне нужно начать заново...")
+    await message.answer("Вот меню:", reply_markup=keyboards.main_menu())
+    await state.set_state(states.CommonStates.main_menu)
--- src/classes/states.py
+++ src/classes/states.py
@@ -4,4 +4,3 @@ from aiogram.fsm.state import StatesGroup, State
 
 class CommonStates(StatesGroup):
     main_menu = State()
-    choosing_food_size = State()
