4ea5cf8b38cec357c3914868dcc0e629b736852f
Hecker <denis.ureev77@gmail.com>
2025-08-01 20:37:59 +0300

    работай тварь

---
+++ src/configs/payments.py
@@ -0,0 +1,44 @@
+from schemas.payment_models import PaymentMethod, PaymentMethodsRepository
+from schemas.types import LocalizedString
+
+SUPPORTED_PAYMENT_METHODS = PaymentMethodsRepository({
+    "manual_sbp": PaymentMethod(
+        name=LocalizedString({
+            "ru": "СБП по номеру телефона",
+            "en": "SBP by phone number"
+        }),
+        description=LocalizedString({
+            "ru": "Тут надо указать реквизиты для проведения оплаты, и что-то типо \"Если вы хотите выбрать данный платежный метод, произведите по нему оплату и нажмите на кнопку такую-то.\"",
+            "en": "DESCRIPTION PLACEHOLDER"
+        }),
+        enabled=True,
+        can_register_receipts=True,
+        manual=True
+    ),
+    "manual_card": PaymentMethod(
+        name=LocalizedString({
+            "ru": "По номеру карты",
+            "en": "By card number"
+        }),
+        description=LocalizedString({
+            "ru": "Тут надо указать реквизиты для проведения оплаты, и что-то типо \"Если вы хотите выбрать данный платежный метод, произведите по нему оплату и нажмите на кнопку такую-то.\"",
+            "en": "DESCRIPTION PLACEHOLDER"
+        }),
+        enabled=True,
+        can_register_receipts=True,
+        manual=True
+    ),
+    "manual_paypal": PaymentMethod(
+        name=LocalizedString({
+            "ru": "PayPal",
+            "en": "PayPal"
+        }),
+        description=LocalizedString({
+            "ru": "Тут надо указать реквизиты для проведения оплаты, и что-то типо \"Если вы хотите выбрать данный платежный метод, произведите по нему оплату и нажмите на кнопку такую-то.\"",
+            "en": "DESCRIPTION PLACEHOLDER"
+        }),
+        enabled=True,
+        can_register_receipts=False,
+        manual=True
+    ),
+})
