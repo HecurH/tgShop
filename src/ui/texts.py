97a6cb95a802806166e05000457ca446848b31da
Hecker <denis.ureev77@gmail.com>
2025-08-17 02:00:18 +0300

    рь

--- src/ui/texts.py
+++ src/ui/texts.py
@@ -170,8 +170,10 @@ class CartTextGen:
             product = await ctx.db.products.find_one_by_id(entry.product_id)
             quantity_text = f" {entry.quantity} {UncategorizedTranslates.translate('unit', ctx.lang, count=entry.quantity)}" if entry.quantity > 1 else ""
             price = product.base_price + entry.configuration.price
+            price_text = price.to_text(ctx.customer.currency)
+            price_text = f"{price_text} * {entry.quantity} = {(price_text*entry.quantity).to_text(ctx.customer.currency)}" if entry.quantity != 1 else price_text
             
-            return f"{product.name.get(ctx.lang)}{quantity_text} — {price.to_text(ctx.customer.currency)}"
+            return f"{product.name.get(ctx.lang)}{quantity_text} — {price_text}"
             
         entries = await ctx.db.cart_entries.get_customer_cart_entries(ctx.customer)
         cart_entries_description = await asyncio.gather(*(form_entry_desc(entry) for entry in entries))
