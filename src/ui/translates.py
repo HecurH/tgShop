6842deb3f52b852e30d63a30f0740fc2fa1c3bb6
Hecker <denis.ureev77@gmail.com>
2025-08-21 14:36:10 +0300

    фикс?

--- src/ui/translates.py
+++ src/ui/translates.py
@@ -5,55 +5,33 @@ from typing import ClassVar, Type
 from configs.supported import SUPPORTED_LANGUAGES_TEXT
 
 class TranslationField:
-    """
-    Дескриптор, который возвращает перевод в зависимости от языка экземпляра.
-    """
-
     def __init__(self, translations: dict):
         self.translations = translations
-        self._attribute_name = None  # имя будет установлено метаклассом
-        self._owner_class = None
+        self._attribute_name = None
+        self._owner_class = None  # сохраним ссылку на класс
 
     def __set_name__(self, owner, name):
-        # сохраняет имя атрибута, к которому привязан дескриптор
         if self._attribute_name is not None:
             raise RuntimeError(
-                f"TranslationField instance already assigned to attribute '{self._attribute_name}'. "
-                f"Do not reuse the same TranslationField instance for multiple attributes/classes."
+                f"TranslationField instance already assigned to attribute '{self._attribute_name}'."
             )
         self._attribute_name = name
         self._owner_class = owner
 
     def __get__(self, instance, owner):
-        # instance - это экземпляр класса, owner - это сам класс
         if instance is None:
-            # если обращаемся к атрибуту через класс, возвращаем сам дескриптор
             return self
 
         lang = instance._lang
-        
-        value = (
-            self.translations.get(lang)
-            or self.translations.get('en')
-            or next(iter(self.translations.values()))
-        )
-        print(type(value))
+        return self.translate(lang)
 
-        if isinstance(value, dict):
-            def pluralizer(count: int):
-                return owner.translate(self._attribute_name, lang, count=count)
-            return pluralizer
-        else:
-            return owner.translate(self._attribute_name, lang)
-    
     def translate(self, lang: str, count: int = None) -> str:
-        if self._owner_class is None:
-            raise RuntimeError("TranslationField has no owner_class yet")
+        cls = self._owner_class
+        return cls.translate(self._attribute_name, lang, count=count)
 
-        return self._owner_class.translate(self._attribute_name, lang, count=count)
-    
     def values(self):
         return self.translations.values()
+
         
         
 class TranslationMeta(type):
