5c22155b0543a9c946ed80420daf4c62bd7f275e
Hecker <denis.ureev77@gmail.com>
2025-08-29 21:42:25 +0300

    позабыл добавить это

--- src/handlers/orders.py
+++ src/handlers/orders.py
@@ -1,4 +1,13 @@
 from aiogram import Router
 
+from core.helper_classes import Context
+from core.states import CommonStates, Orders, call_state_handler
+from ui.translates import ReplyButtonsTranslates
+
 
 router = Router(name="orders")
+
+
+@router.message(CommonStates.MainMenu, lambda message: (message.text in ReplyButtonsTranslates.orders.values()) if message.text else False)
+async def profile_entrance_handler(_, ctx: Context) -> None:
+    await call_state_handler(Orders.Menu, ctx)
