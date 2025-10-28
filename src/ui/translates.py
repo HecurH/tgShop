6de1f1d7be43ef8644447953ab7480af7bbe394b
Hecker <denis.ureev77@gmail.com>
2025-10-29 00:48:21 +0300

    дописка

--- src/ui/translates.py
+++ src/ui/translates.py
@@ -579,7 +579,7 @@ The final cost for a custom paint job is calculated individually and depends on 
                                     "en": "Shimmer"
                                 }
                                 description = {
-                                    "ru": "Представляет собой мелкодисперсный порошок с металлическим сиянием, придающий поверхности интересный перелив и игру света. Является самостоятельным цветом для покраски. (см. Доступные цвета)",
+                                    "ru": "Представляет собой порошковый краситель с металлическим сиянием, придающий поверхности интересный перелив и игру света. Является самостоятельным цветом для покраски. (см. Доступные цвета)",
                                     "en": "It is a fine powder with a metallic sheen, giving the surface an interesting iridescence and a play of light. It is an independent color for painting. (see Available colors)"
                                 }
                             
