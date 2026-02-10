93e03b3251fd674d28e67397e275f01986378c86
Hecker <denis.ureev77@gmail.com>
2026-02-10 21:13:54 +0300

    вебхуки/поллинг выбор

--- src/run.py
+++ src/run.py
@@ -15,7 +15,6 @@ from pymongo import AsyncMongoClient
 project_root = Path(__file__).parent.parent
 sys.path.append(str(project_root))
 
-from core.webhook import create_app
 from handlers import admin_menu, discounted_products, profile, admin, bottom, cart, orders, common, assortment
 from core.logger import setup_logging
 from core import middlewares
@@ -61,13 +60,24 @@ def main() -> None:
     dp.workflow_data["context_middleware"] = context_middleware
     dp.workflow_data["bot"] = bot
     
-    app = create_app(dp, bot)
+    if getenv("USE_WEBHOOK") == 1:
+        from core.webhook import create_app
     
-    web.run_app(app, 
-                host=load_env('WEB_SERVER_HOST'), 
-                port=int(load_env('WEB_SERVER_PORT')),
-                access_log=None,
-                print=None)
+        app = create_app(dp, bot)
+        
+        web.run_app(app, 
+                    host=load_env('WEB_SERVER_HOST'), 
+                    port=int(load_env('WEB_SERVER_PORT')),
+                    access_log=None,
+                    print=None)
+    else:
+        import asyncio
+        
+        async def polling():
+            await bot.delete_webhook(drop_pending_updates=False)
+            await dp.start_polling(bot)
+        
+        asyncio.run(polling())
 
 
 if __name__ == "__main__":
